3a5532beae92f2de05b4c4838e0a95c76f873698
diego <diego@localhost>
2022-02-24 21:20:53 -0500

    44

--- demo1/src/demo1/HelloWorld.java
+++ demo1/src/demo1/HelloWorld.java
@@ -1,10 +1,10 @@
 package demo1;
 
-public class S2 {
+public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 
 }
